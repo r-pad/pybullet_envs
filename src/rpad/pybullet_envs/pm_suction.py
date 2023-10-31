e2c3475b224e0dc9c3bcfb07b0b274d9aac403d8
Ben Eisner <ben.a.eisner@gmail.com>
2023-10-31 19:51:58 -0400

    environment seems to work

--- src/rpad/pybullet_envs/pm_suction.py
+++ src/rpad/pybullet_envs/pm_suction.py
@@ -5,12 +5,22 @@ from typing import List
 
 import gymnasium as gym
 import numpy as np
+import numpy.typing as npt
 import pybullet as p
 from rpad.partnet_mobility_utils.data import PMObject
 from rpad.partnet_mobility_utils.render.pybullet import PMRenderEnv
 from scipy.spatial.transform import Rotation as R
 
 from rpad.pybullet_envs.suction_gripper import FloatingSuctionGripper
+
+
+def reindex(seg, segmap, obj_id):
+    pc_seg_obj = np.ones_like(seg) * -1
+    for k, (body, link) in segmap.items():
+        if body == obj_id:
+            ixs = seg == k
+            pc_seg_obj[ixs] = link
+    return pc_seg_obj
 
 
 class PMSuctionEnv:
@@ -23,6 +33,7 @@ class PMSuctionEnv:
     ) -> None:
         # Create a core environment that we'll manipulate.
         self._core_env = PMRenderEnv(obj_id, pm_dataset_path, camera_pos, gui=gui)
+        self.gui = gui
 
         # Add a suction gripper.
         self.gripper = FloatingSuctionGripper(self._core_env.client_id)
@@ -33,16 +44,18 @@ class PMSuctionEnv:
 
     def teleport_and_approach(self, point, contact_vector, standoff_d: float = 0.2):
         # Normalize contact vector.
-        contact_vector = (contact_vector / contact_vector.norm(dim=-1)).astype(float)
+        contact_vector = (
+            contact_vector / np.linalg.norm(contact_vector, axis=-1)
+        ).astype(float)
 
         p_teleport = (point + contact_vector * standoff_d).astype(float)
 
-        e_z_init = np.asarray([0, 0, 1.0]).astype(float)
+        e_z_init: npt.NDArray[np.float64] = np.asarray([0, 0, 1.0]).astype(float)
         e_y = -contact_vector
         e_x = np.cross(-contact_vector, e_z_init)
-        e_x = e_x / e_x.norm(dim=-1)
+        e_x = e_x / np.linalg.norm(e_x, axis=-1)
         e_z = np.cross(e_x, e_y)
-        e_z = e_z / e_z.norm(dim=-1)
+        e_z = e_z / np.linalg.norm(e_z, axis=-1)
         R_teleport = np.stack([e_x, e_y, e_z], axis=1)
         R_gripper = np.asarray(
             [
@@ -56,17 +69,17 @@ class PMSuctionEnv:
 
         self.gripper.set_pose(p_teleport, o_teleport)
 
-        contact = self.gripper.detect_contact(self.obj_id)
+        contact = self.gripper.detect_contact(self._core_env.obj_id)
         max_steps = 500
         curr_steps = 0
         self.gripper.set_velocity(-contact_vector * 0.4, [0, 0, 0])
         while not contact and curr_steps < max_steps:
-            p.stepSimulation(self.client_id)
+            p.stepSimulation(self._core_env.client_id)
             curr_steps += 1
             if self.gui:
                 time.sleep(1 / 240.0)
             if curr_steps % 10 == 0:
-                contact = self.gripper.detect_contact(self.obj_id)
+                contact = self.gripper.detect_contact(self._core_env.obj_id)
 
         if contact:
             print("contact detected")
@@ -74,7 +87,7 @@ class PMSuctionEnv:
         return contact
 
     def attach(self):
-        self.gripper.activate(self.obj_id)
+        self.gripper.activate(self._core_env.obj_id)
 
     def pull(self, direction, n_steps: int = 100):
         direction = np.asarray(direction)
@@ -82,19 +95,23 @@ class PMSuctionEnv:
         # breakpoint()
         for _ in range(n_steps):
             self.gripper.set_velocity(direction * 0.4, [0, 0, 0])
-            p.stepSimulation(self.client_id)
+            p.stepSimulation(self._core_env.client_id)
             if self.gui:
                 time.sleep(1 / 240.0)
 
     def get_joint_value(self, target_link: str):
-        link_index = self.link_name_to_index[target_link]
-        state = p.getJointState(self.obj_id, link_index, self.client_id)
+        link_index = self._core_env.link_name_to_index[target_link]
+        state = p.getJointState(
+            self._core_env.obj_id, link_index, self._core_env.client_id
+        )
         joint_pos = state[0]
         return joint_pos
 
     def detect_success(self, target_link: str):
-        link_index = self.link_name_to_index[target_link]
-        info = p.getJointInfo(self.obj_id, link_index, self.client_id)
+        link_index = self._core_env.link_name_to_index[target_link]
+        info = p.getJointInfo(
+            self._core_env.obj_id, link_index, self._core_env.client_id
+        )
         lower, upper = info[8], info[9]
         curr_pos = self.get_joint_value(target_link)
 
@@ -140,29 +157,42 @@ def run_trial(
     #     env.set_joint_state(target_link, 0.2)
 
     if raw_data.semantics.by_name(target_link).type == "hinge":
-        env.set_joint_state(target_link, 0.05)
+        target_joint = raw_data.obj.get_joint_by_child(target_link).name
+        env.set_joint_state({target_joint: 0.05})
 
     # Predict the flow on the observation.
     pc_obs = env.render(filter_nonobj_pts=True, n_pts=n_pts)
-    rgb, depth, seg, P_cam, P_world, pc_seg, segmap = pc_obs
-
-    pred_trajectory = model(copy.deepcopy(pc_obs))
-    pred_trajectory = pred_trajectory.reshape(
-        pred_trajectory.shape[0], -1, pred_trajectory.shape[-1]
+    rgb, depth, seg, P_cam, P_world, pc_seg, segmap = (
+        pc_obs["rgb"],
+        pc_obs["depth"],
+        pc_obs["seg"],
+        pc_obs["P_cam"],
+        pc_obs["P_world"],
+        pc_obs["pc_seg"],
+        pc_obs["segmap"],
     )
-    pred_flow = pred_trajectory[:, 0, :]
+    pc_seg = reindex(pc_seg, segmap, env._core_env.obj_id)
+
+    pred_flow = model(copy.deepcopy(pc_obs))
 
     # flow_fig(torch.from_numpy(P_world), pred_flow, sizeref=0.1, use_v2=True).show()
     # breakpoint()
 
     # Filter down just the points on the target link.
-    link_ixs = pc_seg == env.link_name_to_index[target_link]
+    link_ixs = pc_seg == env._core_env.link_name_to_index[target_link]
+
+    # breakpoint()
+
     assert link_ixs.any()
 
     # The attachment point is the point with the highest flow.
-    best_flow_ix = pred_flow[link_ixs].norm(dim=-1).argmax()
+    best_flow_ix = np.linalg.norm(pred_flow[link_ixs], axis=-1).argmax()
     best_flow = pred_flow[link_ixs][best_flow_ix]
     best_point = P_world[link_ixs][best_flow_ix]
+
+    # fig = flow_fig(P_world, pred_flow)
+    # fig.add_trace(vector(*best_point, *best_flow, scene="scene1", color="yellow"))
+    # fig.show()
 
     # Teleport to an approach pose, approach, the object and grasp.
     contact = env.teleport_and_approach(best_point, best_flow)
@@ -190,14 +220,24 @@ def run_trial(
 
         for traj_step in range(traj_len):
             pred_flow = pred_trajectory[:, traj_step, :]
-            rgb, depth, seg, P_cam, P_world, pc_seg, segmap = pc_obs
+
+            rgb, depth, seg, P_cam, P_world, pc_seg, segmap = (
+                pc_obs["rgb"],
+                pc_obs["depth"],
+                pc_obs["seg"],
+                pc_obs["P_cam"],
+                pc_obs["P_world"],
+                pc_obs["pc_seg"],
+                pc_obs["segmap"],
+            )
+            pc_seg = reindex(pc_seg, segmap, env._core_env.obj_id)
 
             # Filter down just the points on the target link.
-            link_ixs = pc_seg == env.link_name_to_index[target_link]
+            link_ixs = pc_seg == env._core_env.link_name_to_index[target_link]
             assert link_ixs.any()
 
             # Get the best direction.
-            best_flow_ix = pred_flow[link_ixs].norm(dim=-1).argmax()
+            best_flow_ix = np.linalg.norm(pred_flow[link_ixs], axis=-1).argmax()
             best_flow = pred_flow[link_ixs][best_flow_ix]
 
             # Perform the pulling.
--- tests/pm_suction_test.py
+++ tests/pm_suction_test.py
@@ -1,11 +1,110 @@
 import os
+from typing import Dict, Literal, Sequence, Union, cast
 
 import numpy as np
 import numpy.typing as npt
+import pybullet as p
 import pytest
+import rpad.partnet_mobility_utils.articulate as pma
 from rpad.partnet_mobility_utils.data import PMObject
 
-from rpad.pybullet_envs.pm_suction import PMSuctionEnv, run_trial
+from rpad.pybullet_envs.pm_suction import PMSuctionEnv, reindex, run_trial
+
+
+# COPIED FROM FLOWBOT, DON'T WANT TO ADD FLOWBOT AS A DEPENDENCY...
+def compute_normalized_flow(
+    P_world: npt.NDArray[np.float32],
+    T_world_base: npt.NDArray[np.float32],
+    current_jas: Dict[str, float],
+    pc_seg: npt.NDArray[np.uint8],
+    labelmap: Dict[str, int],
+    pm_raw_data: PMObject,
+    linknames: Union[Literal["all"], Sequence[str]] = "all",
+) -> npt.NDArray[np.float32]:
+    """Compute normalized flow for an object, based on its kinematics.
+
+    Args:
+        P_world (npt.NDArray[np.float32]): Point cloud render of the object in the world frame.
+        T_world_base (npt.NDArray[np.float32]): The pose of the base link in the world frame.
+        current_jas (Dict[str, float]): The current joint angles (easy to acquire from the render that created the points.)
+        pc_seg (npt.NDArray[np.uint8]): The segmentation labels of each point.
+        labelmap (Dict[str, int]): Map from the link name to segmentation name.
+        pm_raw_data (PMObject): The object description, essentially providing the kinematic structure of the object.
+        linknames (Union[Literal['all'], Sequence[str]], optional): The names of the links for which to
+            compute flow. Defaults to "all", which will articulate all of them.
+
+    Returns:
+        npt.NDArray[np.float32]: _description_
+    """
+
+    # We actuate all links.
+    if linknames == "all":
+        joints = pm_raw_data.semantics.by_type("slider")
+        joints += pm_raw_data.semantics.by_type("hinge")
+        linknames = [joint.name for joint in joints]
+
+    flow = np.zeros_like(P_world)
+
+    for linkname in linknames:
+        P_world_new = pma.articulate_joint(
+            pm_raw_data,
+            current_jas,
+            linkname,
+            0.01,  # Articulate by only a little bit.
+            P_world,
+            pc_seg,
+            labelmap,
+            T_world_base,
+        )
+        link_flow = P_world_new - P_world
+        flow += link_flow
+
+    largest_mag: float = np.linalg.norm(flow, axis=-1).max()
+
+    normalized_flow = flow / (largest_mag + 1e-6)
+
+    return normalized_flow
+
+
+class GTFlowModel:
+    def __init__(self, raw_data, env):
+        self.env = env
+        self.raw_data = raw_data
+
+    def __call__(self, obs) -> npt.NDArray[np.float32]:
+        rgb, depth, seg, P_cam, P_world, pc_seg, segmap = (
+            obs["rgb"],
+            obs["depth"],
+            obs["seg"],
+            obs["P_cam"],
+            obs["P_world"],
+            obs["pc_seg"],
+            obs["segmap"],
+        )
+        pc_seg = reindex(pc_seg, segmap, self.env._core_env.obj_id)
+
+        env = self.env
+        raw_data = self.raw_data
+
+        links = raw_data.semantics.by_type("slider")
+        links += raw_data.semantics.by_type("hinge")
+        current_jas: Dict[str, float] = {}
+        for link in links:
+            linkname = link.name
+            chain = raw_data.obj.get_chain(linkname)
+            for joint in chain:
+                current_jas[joint.name] = 0
+        normalized_flow = compute_normalized_flow(
+            P_world,
+            env._core_env.T_world_base,
+            current_jas,
+            pc_seg,
+            env._core_env.link_name_to_index,
+            raw_data,
+            "all",
+        )
+
+        return normalized_flow
 
 
 class RandomFlowModel:
@@ -14,9 +113,17 @@ class RandomFlowModel:
         self.raw_data = raw_data
 
     def __call__(self, obs) -> npt.NDArray[np.float64]:
-        rgb, depth, seg, P_cam, P_world, pc_seg, segmap = obs
+        rgb, depth, seg, P_cam, P_world, pc_seg, segmap = (
+            obs["rgb"],
+            obs["depth"],
+            obs["seg"],
+            obs["P_cam"],
+            obs["P_world"],
+            obs["pc_seg"],
+            obs["segmap"],
+        )
 
-        return np.random.random(P_cam.shape)
+        return cast(npt.NDArray[np.float64], np.random.random(P_cam.shape))
 
 
 @pytest.mark.skipif(
@@ -33,9 +140,16 @@ def test_pm_suction():
         "slider"
     )
 
+    seed = 123
+    np.random.seed(seed)
     joint = available_joints[np.random.randint(0, len(available_joints))]
-    model = RandomFlowModel(raw_data, env)
+    # model = RandomFlowModel(raw_data, env)
+    model = GTFlowModel(raw_data, env)
 
     # t0 = time.perf_counter()
     print(f"opening {joint.name}, {joint.label}")
-    return run_trial(env, raw_data, joint.name, model, n_steps=10)
+    res = run_trial(env, raw_data, joint.name, model, n_steps=10)
+
+    assert res.success
+
+    p.disconnect(env._core_env.client_id)

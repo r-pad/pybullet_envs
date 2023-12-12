2c442d19b036d0a8baaf63e253ae37163141738e
AkshayLaddha943 <akshaymahesh10@gmail.com>
2023-12-12 20:46:57 +0530

    grasping flying gripper test

--- src/rpad/pybullet_envs/parallel_jaw_v2.py
+++ src/rpad/pybullet_envs/parallel_jaw_v2.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 try:
     from importlib.resources import as_file, files  # type: ignore
@@ -13,6 +14,7 @@ fn = as_file(files("rpad_pybullet_envs_where2act_data").joinpath("."))
 with fn as f:
     PARALLEL_JAW_GRIPPER_URDF = os.path.join(f, "panda_gripper.urdf")
 
+
 class FloatingParallelJawGripper:
     def __init__(self, client_id):
         self.client_id = client_id
@@ -22,3 +24,96 @@ class FloatingParallelJawGripper:
         self.base_id = p.loadURDF(
             PARALLEL_JAW_GRIPPER_URDF, *base_pose, physicsClientId=self.client_id
         )
+        
+        self.mass = 5
+        
+        self.activated = False
+        self.contact_const = None
+        self.contact_link_index = None
+        
+    def set_pose(self, pos, ori):
+        p.resetBasePositionAndOrientation(self.base_id, pos, ori, self.client_id)
+    
+    def set_velocity(self, lin_vel, ang_vel):
+         p.resetBaseVelocity(self.base_id, lin_vel, ang_vel, self.client_id)
+         
+    def detect_contact(self, obj_id):
+        points = p.getContactPoints(bodyA=self.base_id, bodyB=obj_id, linkIndexA=0)
+        return len(points) != 0
+        
+    
+    def grasp(self, obj_id):
+        if not self.activated:          
+            contact_pts = p.getContactPoints(
+                bodyA = self.base_id,
+                bodyB = obj_id,
+                linkIndexA = 0,
+            )
+            
+            if contact_pts:
+                contact_point = contact_pts[0]
+                
+                #gripper link
+                gripper_link_pos, gripper_link_ori, _,_,_ = p.getLinkState(
+                    bodyUniqueID=self.base_id,
+                    linkIndex=0,
+                    computeLinkVelocity=0,
+                    physicsClientId=self.client_id
+                )
+                
+                obj_link_pos, obj_link_ori, _, _, _, _ = p.getLinkState(
+                    bodyUniqueId=obj_id,
+                    linkIndex=contact_point[4],
+                    computeLinkVelocity=0,
+                    physicsClientId=self.client_id,
+                )
+                
+                #transform contact pts in gripper link frame
+                contact_pos_world = contact_point[5]
+                contact_ori_world = gripper_link_ori
+                contact_pos_gripper = p.invertTransform(
+                    gripper_link_pos, gripper_link_ori, contact_pos_world
+                )
+                
+                self.contact_const = p.createConstraint(
+                    parentBodyUniqueId=self.base_id,
+                    parentLinkIndex=0,
+                    childBodyUniqueId=obj_id,
+                    childLinkIndex=contact_point[4],
+                    jointType=p.JOINT_FIXED,
+                    jointAxis=(0,0,0),
+                    parentFramePosition=contact_pos_gripper,
+                    parentFrameOrientation=gripper_link_ori,
+                    childFramePosition=contact_point[6],
+                    childFrameOrientation=obj_link_ori,
+                    physicsClientId=self.client_id
+                )
+                
+                self.activated=True
+                self.contact_link_index = 0
+    
+    
+    def apply_force(self, force):
+        body_link_pos, body_link_ori, _, _, _, _ = p.getLinkState(
+            bodyUniqueId=self.base_id,
+            linkIndex=0,
+            computeLinkVelocity=0,
+            physicsClientId=self.client_id,
+        )
+        
+        p.applyExternalForce(
+            self.base_id,
+            linkIndex=0,
+            forceObj=force,
+            posObj=body_link_pos,
+            flags=p.WORLD_FRAME,
+            physicsClientId=self.client_id,
+        )
+    
+    
+    def release(self):
+        if self.contact_const:
+            p.removeConstraint(self.contact_const)
+        self.activated = False
+        self.contact_link_index = None
+                
--- tests/parallel_jaw_gripper_test.py
+++ tests/parallel_jaw_gripper_test.py
@@ -1,5 +1,8 @@
 import pybullet as p
+import pybullet_data
+import numpy as np
 
+#from rpad.pybullet_envs.pm_parallel_jaw import PMFloatingEnv
 from rpad.pybullet_envs.parallel_jaw_v2 import FloatingParallelJawGripper
 
 
@@ -13,5 +16,122 @@ def test_parallel_jaw_gripper_creation():
 
 def test_parallel_jaw_gripper():
     client_id = p.connect(p.DIRECT)
+    
+    gripper = FloatingParallelJawGripper(client_id)
+    
+    num_joints = p.getNumJoints(gripper.base_id)
+    joint_indices = [i for i in range(num_joints)]
 
-    gripper = FloatingParallelJawGripper()
+    initial_joint_angles = [0.0] * num_joints
+    p.setJointMotorControlArray(
+        bodyUniqueId=gripper.base_id,
+        jointIndices=joint_indices,
+        controlMode=p.POSITION_CONTROL,
+        targetPositions=initial_joint_angles,
+    )
+    
+    target_joint_angles = [0.25] * num_joints
+
+    p.setJointMotorControlArray(
+        bodyUniqueId=gripper.base_id,
+        jointIndices=joint_indices,
+        controlMode=p.POSITION_CONTROL,
+        targetPositions=target_joint_angles,
+        forces=[10.0] * num_joints,  # Adjust force as needed
+    )
+
+    # Run the simulation for a few steps
+    for _ in range(100):
+        p.stepSimulation()
+        
+
+def test_parallel_jaw_gripper_grasping():
+    
+    client_id = p.connect(p.GUI)
+    
+    p.resetDebugVisualizerCamera(
+            cameraDistance=1.5,
+            cameraYaw=30,
+            cameraPitch=-20,
+            cameraTargetPosition=[0, 0, 0.5],
+        )
+    
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    plane_id = p.loadURDF("plane.urdf", physicsClientId=client_id)
+    cube_id = p.loadURDF("C:\pybullet_envs\models\cube.urdf", physicsClientId=client_id)
+    
+    #placing cube above origin
+    p.resetBasePositionAndOrientation(cube_id, [0, 0, 1], [0, 0, 0, 1], client_id)
+    
+    # Turn on gravity.
+    p.setGravity(0, 0, -9.81, client_id)
+    
+    # Step the sim and wait for it to settle
+    for _ in range(1000):
+        p.stepSimulation(client_id)
+        
+    # Turn off gravity.
+    p.setGravity(0, 0, 0, client_id)
+    
+    # Get the settled position of the cube.
+    start_pos, _ = p.getBasePositionAndOrientation(cube_id, client_id)
+    
+    # Create a parallel-jaw gripper.
+    gripper = FloatingParallelJawGripper(client_id)
+    
+    # Put the parallel-jaw gripper above the cube.
+    gripper.set_pose([0, 0, 0.25], p.getQuaternionFromEuler([np.pi, 0, 0]))
+    
+    # Set the velocity of the gripper to be downward.
+    gripper.set_velocity([0, 0, -0.1], [0, 0, 0])
+    
+    # Step the simulation.
+    for i in range(20000):
+        p.stepSimulation(client_id)
+        # Detect contact.
+        if gripper.detect_contact(cube_id):
+            print("Contact detected!")
+            break
+    
+    if i >= 20000:
+        raise ValueError("No contact detected!")
+    
+    # Grasp the cube with the parallel-jaw gripper.
+    gripper.grasp(cube_id)
+    
+    # Set the velocity of the gripper to be upward.
+    gripper.set_velocity([0, 0, 0.1], [0, 0, 0])
+    
+    # Step the simulation.
+    for _ in range(20000):
+        p.stepSimulation(client_id)
+        
+    # Make sure that the cube is above the ground.
+    # apex_pos, _ = p.getBasePositionAndOrientation(cube_id, client_id)
+    # if apex_pos[2] - start_pos[2] < 0.1:
+    #     raise ValueError("Cube is not off the ground!")
+    
+    # Enable gravity.
+    p.setGravity(0, 0, -9.81, client_id)
+    
+    # Release the grip
+    gripper.release()
+    
+    # Step the simulation.
+    for _ in range(5000):
+        gripper.apply_force([0, 0, 9.81 * gripper.mass + 3])
+        p.stepSimulation(client_id)
+
+        
+# def partnet_parallel_jaw_test():
+#     client_id = p.connect(p.GUI)
+
+#     gripper = PMFloatingEnv(client_id)
+
+#     #p.disconnect(client_id)
+
+
+if __name__=="__main__":
+    test_parallel_jaw_gripper_grasping()
+    while True:
+        pass
